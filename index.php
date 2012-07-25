e89ddf72be06c9de2e5bf5da656aa781161f4b2c
Filip Zajac <filipkoxy@gmail.com>
2012-07-25 19:28:18 +0200

    Nova verzia style up date
    Spodny panel
    do index.php pridany datum
    
    to do:
    Treba doriesit fotky a obsah ktory nacitava ten skript, Napr H1
    nacitava aj do policok, ked sa zmaze nejde ani v nahlade ani v policku.

--- index.php
+++ index.php
@@ -19,18 +19,19 @@
 <ul id="locations">
 
 <?php while ( have_posts() ) : the_post() ?>
-
-
 <li data-kml="<?php if (get_post_custom_values('kml')) { $myKml = get_post_custom_values('kml'); echo $myKml[0]; } ?>">
+		
 		<h2 class="entry-title">
 			<a href="<?php the_permalink(); ?>" title="<?php printf( __('Permalink to %s', 'wandrak-02'), the_title_attribute('echo=0') ); ?>">
 				<?php printf( __('Permalink to %s', 'wandrak-02'), the_title_attribute('echo=0') ); ?>
 			</a>
 		</h2>
+		
 		<div class="entry-meta">
-				<span class="author vcard"><a class="url fn n" href="<?php echo get_author_link( false, $authordata->ID, $authordata->user_nicename ); ?>" title="<?php printf( __( 'View all posts by %s', 'wandrak-02' ), $authordata->display_name ); ?>"><?php the_author(); ?></a></span>
-				<span class="meta-sep"></span>
-				<span class="meta-prep meta-prep-entry-date"><?php _e('', 'wandrak-02'); ?></span>
+				
+				<span class="post-month"><?php the_time('M') ?></span>
+				<span class="post-day"><?php the_time('d') ?></span>
+				
 				<div class="longdesc"><?php the_content( __('Continue Reading &rarr;','wandrak-02' ) ); ?>
 				<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'wandrak-02' ) . '&after=</div>') ?></div>
 		</div><!-- .entry-meta -->
@@ -48,7 +49,7 @@
 </div>
 </div>
 <div id="more-info">
-					<h2></h2>
+					<h2 class="preview"></h2>
 					<div class="excerpt"></div>
 </div>			
 			</div><!-- #content -->		
