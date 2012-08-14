ab38a9db8f246fc38278cc30050ce91feda6e372
Stefan <stephan>
2012-08-14 17:54:59 +0200

    markers with custom icons

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 	var bottomBarApi;
 	var scrolledToLast = false;
 
+	var image = new google.maps.MarkerImage('images/beachflag.png',
+      // This marker is 20 pixels wide by 32 pixels tall.
+      new google.maps.Size(20, 32),
+      // The origin for this image is 0,0.
+      new google.maps.Point(0,0),
+      // The anchor for this image is the base of the flagpole at 0,32.
+      new google.maps.Point(0, 32));
+
     $(function() {
 		$("#bottom_bar_left_area").click(function () {
 			if (0 <= scrollingIndex - 1 && !scrolledToLast) {
